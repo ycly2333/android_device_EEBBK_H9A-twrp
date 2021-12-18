9b3e8bfa33dd028275c5614fcf8d17925f07274b
ycly2333 <2780870183@qq.com>
2021-12-18 23:38:17 +0800

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_H9A.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_H9A-user \
+    omni_H9A-userdebug \
+    omni_H9A-eng
---
+++ omni_H9A.mk
@@ -0,0 +1,9 @@
+# Inherit from H9A device
+$(call inherit-product, device/EEBBK/H9A/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := H9A
+PRODUCT_NAME := omni_H9A
+PRODUCT_BRAND := EEBBL
+PRODUCT_MODEL := H9A
+PRODUCT_MANUFACTURER := EEBBK
