611a73038c5c0f55ffc2f1035431d17dcba319fd
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2022-02-06 22:19:27 +0800

    Update omni_H9A.mk

--- omni_H9A.mk
+++ omni_H9A.mk
@@ -4,6 +4,6 @@ $(call inherit-product, device/EEBBK/H9A/device.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := H9A
 PRODUCT_NAME := omni_H9A
-PRODUCT_BRAND := EEBBL
+PRODUCT_BRAND := EEBBK
 PRODUCT_MODEL := H9A
 PRODUCT_MANUFACTURER := EEBBK
